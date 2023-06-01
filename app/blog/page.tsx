3c8a2bf06c34cfe7aec93039c5cebc27dd77f76b
Chingiz Mamedov <38461842+chingizmamedov@users.noreply.github.com>
2023-06-01 04:49:47 +0400

    Make most liked as swiper

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,5 @@
 import Container from "@components/Container";
+import MostLikedPosts from "@components/MostLikedPosts";
 import Link from "next/link";
 import React from "react";
 
@@ -61,102 +62,63 @@ export default function BlogPage() {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-4 gap-3">
+        <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-3">
           <div className="col-span-3">
             <Post
               title="How create-react-app works under the hood"
-              description="I&apos;m a full-stack developer with a passion for building beautiful, functional, and accessible websites and applications. I&apos;m currently working as a software engineer at a startup called <a href=&apos;https://www.heyorca.com/&apos;>HeyOrca</a> in St. John&apos;s, Newfoundland. I&apos;m also a student at Memorial University of Newfoundland, where I&apos;m pursuing a Bachelor of Science in Computer Science."
+              description="I'm a full-stack developer with a passion for building beautiful, functional, and accessible websites and applications. I'm currently working as a software engineer at a startup called <a href='https://www.heyorca.com/'>HeyOrca</a> in St. John's, Newfoundland. I'm also a student at Memorial University of Newfoundland, where I'm pursuing a Bachelor of Science in Computer Science."
               date="2021-05-01"
               slug="about"
             />
             <Post
               title="How custom hooks work under the hood"
-              description="I know how to use a lot of different technologies, but I&apos;m most comfortable with React, TypeScript, and Node.js. I&apos;m also familiar with Python, Java, and C. I&apos;m always looking to learn new things, and I&apos;m currently learning Rust and Go."
+              description="I know how to use a lot of different technologies, but I'm most comfortable with React, TypeScript, and Node.js. I'm also familiar with Python, Java, and C. I'm always looking to learn new things, and I'm currently learning Rust and Go."
               date="2021-05-01"
               slug="about"
             />
             <Post
               title="How create-react-app works under the hood"
-              description="I&apos;m a full-stack developer with a passion for building beautiful, functional, and accessible websites and applications. I&apos;m currently working as a software engineer at a startup called <a href=&apos;https://www.heyorca.com/&apos;>HeyOrca</a> in St. John&apos;s, Newfoundland. I&apos;m also a student at Memorial University of Newfoundland, where I&apos;m pursuing a Bachelor of Science in Computer Science."
+              description="I'm a full-stack developer with a passion for building beautiful, functional, and accessible websites and applications. I'm currently working as a software engineer at a startup called <a href='https://www.heyorca.com/'>HeyOrca</a> in St. John's, Newfoundland. I'm also a student at Memorial University of Newfoundland, where I'm pursuing a Bachelor of Science in Computer Science."
               date="2021-05-01"
               slug="about"
             />
             <Post
               title="How custom hooks work under the hood"
-              description="I know how to use a lot of different technologies, but I&apos;m most comfortable with React, TypeScript, and Node.js. I&apos;m also familiar with Python, Java, and C. I&apos;m always looking to learn new things, and I&apos;m currently learning Rust and Go."
+              description="I know how to use a lot of different technologies, but I'm most comfortable with React, TypeScript, and Node.js. I'm also familiar with Python, Java, and C. I'm always looking to learn new things, and I'm currently learning Rust and Go."
               date="2021-05-01"
               slug="about"
             />
             <Post
               title="How create-react-app works under the hood"
-              description="I&apos;m a full-stack developer with a passion for building beautiful, functional, and accessible websites and applications. I&apos;m currently working as a software engineer at a startup called <a href=&apos;https://www.heyorca.com/&apos;>HeyOrca</a> in St. John&apos;s, Newfoundland. I&apos;m also a student at Memorial University of Newfoundland, where I&apos;m pursuing a Bachelor of Science in Computer Science."
+              description="I'm a full-stack developer with a passion for building beautiful, functional, and accessible websites and applications. I'm currently working as a software engineer at a startup called <a href='https://www.heyorca.com/'>HeyOrca</a> in St. John's, Newfoundland. I'm also a student at Memorial University of Newfoundland, where I'm pursuing a Bachelor of Science in Computer Science."
               date="2021-05-01"
               slug="about"
             />
             <Post
               title="How custom hooks work under the hood"
-              description="I know how to use a lot of different technologies, but I&apos;m most comfortable with React, TypeScript, and Node.js. I&apos;m also familiar with Python, Java, and C. I&apos;m always looking to learn new things, and I&apos;m currently learning Rust and Go."
+              description="I know how to use a lot of different technologies, but I'm most comfortable with React, TypeScript, and Node.js. I'm also familiar with Python, Java, and C. I'm always looking to learn new things, and I'm currently learning Rust and Go."
               date="2021-05-01"
               slug="about"
             />
             <Post
               title="How create-react-app works under the hood"
-              description="I&apos;m a full-stack developer with a passion for building beautiful, functional, and accessible websites and applications. I&apos;m currently working as a software engineer at a startup called <a href=&apos;https://www.heyorca.com/&apos;>HeyOrca</a> in St. John&apos;s, Newfoundland. I&apos;m also a student at Memorial University of Newfoundland, where I&apos;m pursuing a Bachelor of Science in Computer Science."
+              description="I'm a full-stack developer with a passion for building beautiful, functional, and accessible websites and applications. I'm currently working as a software engineer at a startup called <a href='https://www.heyorca.com/'>HeyOrca</a> in St. John's, Newfoundland. I'm also a student at Memorial University of Newfoundland, where I'm pursuing a Bachelor of Science in Computer Science."
               date="2021-05-01"
               slug="about"
             />
             <Post
               title="How custom hooks work under the hood"
-              description="I know how to use a lot of different technologies, but I&apos;m most comfortable with React, TypeScript, and Node.js. I&apos;m also familiar with Python, Java, and C. I&apos;m always looking to learn new things, and I&apos;m currently learning Rust and Go."
+              description="I know how to use a lot of different technologies, but I'm most comfortable with React, TypeScript, and Node.js. I'm also familiar with Python, Java, and C. I'm always looking to learn new things, and I'm currently learning Rust and Go."
               date="2021-05-01"
               slug="about"
             />
           </div>
-          <div className="col-span-1">
-            <div className="sticky top-0">
-              <div>
-                <h4 className="text-2xl font-bold text-zinc-950 dark:text-gray-200 mb-3">
-                  Most liked
-                </h4>
-                <div className="flex flex-col gap-2">
-                  <div className="flex flex-col justify-center items-start mb-6 p-3 bg-zinc-800/40 rounded-xl backdrop-blur-lg">
-                    <div className="flex flex-col">
-                      <h3 className="font-bold text-md md:text-xl tracking-tight mb-4 text-black dark:text-white">
-                        How create-react-app works under the hood
-                      </h3>
-                      <p className="text-gray-300 text-sm line-clamp-3">
-                        I&apos;m a full-stack developer with a passion for building
-                        beautiful, functional, and accessible websites and
-                        applications. I&apos;m currently working as a software
-                        engineer at a startup called{" "}
-                        <a
-                          className="text-blue-300"
-                          href="https://www.heyorca.com/"
-                        >
-                          HeyOrca
-                        </a>{" "}
-                        in St. John&apos;s, Newfoundland. I&apos;m also a student at
-                        Memorial University of Newfoundland, where I&apos;m pursuing
-                        a Bachelor of Science in Computer Science.
-                      </p>
-                      <div className="flex gap-2 mt-3">
-                        <div className="tag dark:bg-yellow-500 inline-block rounded-md px-1">
-                          <span className="text-xs">JavaScript</span>
-                        </div>
-                        <div className="tag dark:bg-blue-500 inline-block rounded-md px-1">
-                          <span className="text-xs">ReactJS</span>
-                        </div>
-                      </div>
-                      <div>
-                        <span className="text-xs text-gray-200">
-                          2021-05-01
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+          <div className="col-span-1 order-first lg:order-2">
+            <div className="sticky top-0 w-full">
+              <div className="w-full">
+                <MostLikedPosts />
               </div>
-              <div>
+              <div className="hidden lg:block">
                 <h4 className="text-2xl font-bold text-zinc-950 dark:text-gray-200 mb-3">
                   Popular tags
                 </h4>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper";
 
 import "swiper/css";
 import "swiper/css/pagination";
